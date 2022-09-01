9524e0307ed59cf7c415a05536cf595e3ecca796
JINX-D <111497834+JINX-D@users.noreply.github.com>
2022-09-01 00:33:56 -0700

    Add files via upload

---
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    public Vector3 offset = new Vector3(0, 0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Offset the camera beside the player by adding to the plane's position
+        transform.position = plane.transform.position + offset;
+    }
+}
